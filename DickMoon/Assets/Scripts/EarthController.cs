5f13afbab54df4fccf6347bd4875b7c3a3518a88
Isaac <isaac.james@wbgames.com>
2017-01-20 21:33:23 -0800

    Rotating Earth

---
+++ DickMoon/Assets/Scripts/EarthController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthController : MonoBehaviour
+{
+	public static readonly float ROTATION_RATE = Mathf.PI / 9f * Mathf.Rad2Deg;
+
+	public void Update()
+	{
+		transform.rotation = Quaternion.Euler(0f, 0f, ROTATION_RATE * Time.timeSinceLevelLoad);
+	}
+}
