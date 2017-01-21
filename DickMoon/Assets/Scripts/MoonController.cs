5bd2d21745adc585474074a60d6450ecd03d4395
Isaac <isaac.james@wbgames.com>
2017-01-20 21:07:28 -0800

    Basic moon controls

---
+++ DickMoon/Assets/Scripts/MoonController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonController : MonoBehaviour
+{
+	public static readonly float MAX_HEIGHT = 652f;
+	public static readonly float MIN_HEIGHT = -180f;
+	public static readonly float MAX_VERTICAL_SPEED = 1000f;
+	public static readonly float LERP_TIME = 0.4f;
+
+	protected float speed;
+
+	public void Update()
+	{
+		float targetHeight = MAX_HEIGHT;
+		if (Input.GetKey(KeyCode.Space))
+		{
+			targetHeight = MIN_HEIGHT;
+		}
+
+		Vector3 position = transform.position;
+		position.y = Mathf.SmoothDamp(position.y, targetHeight, ref speed, LERP_TIME, MAX_VERTICAL_SPEED);
+		transform.position = position;
+	}
+}
