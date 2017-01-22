ca6ecde98dcbd07f528d59f2fc94b6b17d48bb91
Isaac <isaac.james@wbgames.com>
2017-01-21 22:10:10 -0800

    New FTUE

---
+++ DickMoon/Assets/Scripts/FTUEController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FTUEController : MonoBehaviour
+{
+	public Image FTUEScreen;
+
+	public Sprite[] Screens;
+
+	protected int index = 0;
+
+	protected void SetScreen(int index)
+	{
+		this.index = index;
+		FTUEScreen.sprite = Screens[index];
+	}
+
+	public void Awake()
+	{
+		SetScreen(0);
+	}
+
+	public void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (++index < Screens.Length)
+			{
+				SetScreen(index);
+			}
+			else
+			{
+				SceneManager.LoadScene("Play");
+			}
+		}
+	}
+}
