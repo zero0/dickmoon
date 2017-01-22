b44751e63f1d3bcbbbbb64fbcd73a9588f4a7422
justin <jrs8212@gmail.com>
2017-01-21 23:50:41 -0800

    - Added new grass - Added other land and monuments to Play scene

--- DickMoon/Assets/Scripts/WaveManager.cs
+++ DickMoon/Assets/Scripts/WaveManager.cs
@@ -6,6 +6,7 @@ public class WaveManager : MonoBehaviour
     public GameObject wavePrefab;
     public float waveSpawnDelay;
     public Transform waveCenter;
+    public Transform moon;
 
     private float _waveSpawnTimer;
 
@@ -20,8 +21,8 @@ public class WaveManager : MonoBehaviour
 
         go1.transform.SetParent(transform.parent, false);
         go2.transform.SetParent(transform.parent, false);
-        go1.transform.localPosition = position;
-        go2.transform.localPosition = position;
+        go1.transform.position = position;
+        go2.transform.position = position;
 
         WaveController w1 = go1.GetComponent<WaveController>();
         WaveController w2 = go1.GetComponent<WaveController>();
@@ -50,7 +51,92 @@ public class WaveManager : MonoBehaviour
 
         if( Input.GetKeyDown( KeyCode.W ) )
         {
-            SpawnWave(new Vector3(905, -597, 0), waveCenter, 100, 2, 4);
+            Vector3 p, n;
+            if( TryMoonPlanetIntersection( moon.transform.position, moon.transform.right, waveCenter.position, 3000, out p, out n ) )
+            {
+                SpawnWave( transform.position, waveCenter, 100, 2, 4);
+            }
         }
+    }
+
+    public static bool TryMoonPlanetIntersection(Vector3 moonPosition, Vector3 moonDirection, Vector3 planetCenter, float planetRadius, out Vector3 intersectionPoint, out Vector3 intersectionNormal)
+    {
+        intersectionPoint = Vector3.zero;
+        intersectionNormal = Vector3.zero;
+
+        Vector3 m = planetCenter - moonPosition;
+        Vector3 d = moonDirection;
+
+        float b = 2 * Vector3.Dot(m, d);
+
+        float c = Vector3.Dot(m, m) - (planetRadius * planetRadius);
+
+        float disc = (b * b) - (4 * c);
+
+        if( disc < 0 )
+        {
+            return false;
+        }
+
+        disc = Mathf.Sqrt(disc);
+
+        float t = (-b - disc) / 2f;
+        if( t < 0 )
+        {
+            t = (-b + disc) / 2f;
+        }
+
+        if( t < 0 )
+        {
+            return false;
+        }
+
+        intersectionPoint = moonPosition + t * moonDirection;
+        intersectionNormal = (intersectionPoint - planetCenter).normalized;
+
+        //float b = Vector3.Dot(m, d);
+        //float c = Vector3.Dot(m, m) - (planetRadius * planetRadius);
+        //
+        //if (c > 0 && b > 0) return false;
+        //
+        //float discr = b * b - c;
+        //
+        //if( discr < 0 )
+        //{
+        //    return false;
+        //}
+        //
+        //float t = -b - Mathf.Sqrt(discr);
+        //
+        //if( t < 0 )
+        //{
+        //    t = 0;
+        //}
+        //intersectionPoint = planetCenter + t * d;
+
+        //float pDotD = Vector3.Dot(m, d);
+        //float rSqr = planetRadius * planetRadius;
+        //
+        //if (pDotD < 0 || Vector3.Dot(m, m) < rSqr)
+        //{
+        //    return false;
+        //}
+        //
+        //Vector3 a = m - d * pDotD;
+        //float aDotA = Vector3.Dot(a, a);
+        //
+        //if (aDotA > rSqr)
+        //{
+        //    return false;
+        //}
+        //
+        //float h = Mathf.Sqrt(rSqr - aDotA);
+        //
+        //Vector3 i = a - h * d;
+        //
+        //intersectionPoint = planetCenter + i;
+        //intersectionNormal = i / planetRadius;
+
+        return true;
     }
 }
