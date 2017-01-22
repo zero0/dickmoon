a5ff527ad6ebb2c2d994999600c6e91766b65d06
Isaac <isaac.james@wbgames.com>
2017-01-22 12:44:14 -0800

    Time speedup

--- DickMoon/Assets/Scripts/ScoreManager.cs
+++ DickMoon/Assets/Scripts/ScoreManager.cs
@@ -5,8 +5,17 @@ using System.Collections;
 public class ScoreManager : MonoBehaviour
 {
 	public static readonly float READ_TIME = 1.3f;
+	public static readonly float LOG_BASE = 8; // Higher means slower speed ramp
 	
 	public static int Score;
+
+	public static float speedMultiplier
+	{
+		get
+		{
+			return Mathf.Log(Mathf.Max(Score / 1000f, 0f) + LOG_BASE) / Mathf.Log(LOG_BASE);
+		}
+	}
 
 	protected static ScoreManager instance;
 	
@@ -24,6 +33,10 @@ public class ScoreManager : MonoBehaviour
 	public static void MakeScore(int score, string notifText)
 	{
 		Score += score;
+		if (Time.timeScale != 0.3f)
+		{
+			Time.timeScale = speedMultiplier;
+		}
 		instance.ScorePanel.text = Score.ToString( "N0" );
 		instance.FlyupPrefab.ScoreNotif.text = notifText;
 		instance.FlyupPrefab.ScoreNotif.enabled = true;
