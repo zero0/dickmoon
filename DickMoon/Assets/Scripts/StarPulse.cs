99fd1cb7521d32200f1c62aeec564e12dd607c2c
Justin <jrs8212@gmail.com>
2017-01-22 10:25:18 -0800

    - Added sadmoon and star assets - Added pulsing stars to Play scene

---
+++ DickMoon/Assets/Scripts/StarPulse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StarPulse : MonoBehaviour
+{
+    public Image star;
+    public float pulseSpeed;
+
+    private float _timeOffset;
+
+    protected void Start()
+    {
+        _timeOffset = Random.Range( -1f, 1f );
+    }
+
+    protected void Update()
+    {
+        _timeOffset += pulseSpeed * Time.deltaTime;
+        float a = Mathf.Sin( _timeOffset );
+
+        Color c = star.color;
+        c.a = a;
+        star.color = c;
+    }
+}
