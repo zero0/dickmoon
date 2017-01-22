3182f66d9ddaf41f80aae4ab93e0aacfa3dee00b
justin <jrs8212@gmail.com>
2017-01-21 21:12:25 -0800

    - Added Wave prefab - Added WaveController to handle making the wave travel and listen for collision events with objects - Added WaveManager to handle spawning waves - Added box collider to LargePyramid prefab so it can collide with waves

---
+++ DickMoon/Assets/Scripts/WaveController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveController : MonoBehaviour
+{
+    public float lifetime = 5;
+    public float speed = 1;
+    public float waveHeight = 1;
+    public Transform center = null;
+
+    protected void Start()
+    {
+    }
+
+    protected void Update()
+    {
+        if (center != null)
+        {
+            Vector3 dir = Vector3.Normalize(center.position - transform.position);
+            Vector3 tangent = Vector3.Cross(dir, Vector3.forward * Mathf.Sign(speed));
+            transform.right = tangent;
+            transform.position += (tangent * Mathf.Abs(speed) * Time.deltaTime);
+
+        }
+
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("Collision Enter " + gameObject.name + " -> " + coll.gameObject.name);
+    }
+
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        Debug.Log("Collision Enter " + gameObject.name + " -> " + coll.gameObject.name);
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        Debug.Log("Collision Exit " + gameObject.name + " -> " + coll.gameObject.name);
+    }
+}
---
+++ DickMoon/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveManager : MonoBehaviour
+{
+    public GameObject wavePrefab;
+    public float waveSpawnDelay;
+
+    private float _waveSpawnTimer;
+
+    public bool SpawnWave( Vector3 position, Transform pivot, float speed, float lifetime )
+    {
+        if (_waveSpawnTimer > 0) return false;
+
+        _waveSpawnTimer = waveSpawnDelay;
+
+        GameObject go1 = GameObject.Instantiate(wavePrefab);
+        GameObject go2 = GameObject.Instantiate(wavePrefab);
+
+        WaveController w1 = go1.GetComponent<WaveController>();
+        WaveController w2 = go1.GetComponent<WaveController>();
+
+        w1.transform.position = w2.transform.position = position;
+
+        w1.center = w2.center = pivot;
+
+        w1.speed = speed;
+        w2.speed = -speed;
+
+        w1.lifetime = w2.lifetime = lifetime;
+
+        return true;
+    }
+
+    protected void Update()
+    {
+        if( _waveSpawnTimer > 0 )
+        {
+            _waveSpawnTimer -= Time.deltaTime;
+        }
+    }
+}
