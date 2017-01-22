50d09b2193a12e2be29eaea60fcfadaecd2b52a9
Isaac <isaac.james@wbgames.com>
2017-01-22 11:49:02 -0800

    Don't show notifs every time

--- DickMoon/Assets/Scripts/MonumentController.cs
+++ DickMoon/Assets/Scripts/MonumentController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MonumentController : MonoBehaviour
 {
+	public static readonly float SHOUT_LIKELINESS = 0.6f;
+	
     public float health = 3;
     public int score;
     public string destroyText;
@@ -16,7 +18,7 @@ public class MonumentController : MonoBehaviour
 			WaveSound.Play();
             transform.Rotate( 0, 0, -80 );
             GetComponent< BoxCollider2D >().enabled = false;
-            ScoreManager.MakeScore(score, destroyText);
+			ScoreManager.MakeScore(score, Random.Range(0f, 1f) < SHOUT_LIKELINESS ? destroyText : "");
         //}
     }
 }
